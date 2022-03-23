cfdeeb6afe0cd2ef647bc81ad9155a73fb23f458
Théo Pomies <theo.pomies@outlook.com>
2022-03-23 16:24:25 +0100

    feat: add: new solution to gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
-use chrono::{DateTime, Duration, Utc};
+use time::ext::NumericalDuration;
+use time::PrimitiveDateTime as DateTime;
 
 // Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1_000_000_000)
+pub fn after(start: DateTime) -> DateTime {
+    start + 1e9.seconds()
 }
