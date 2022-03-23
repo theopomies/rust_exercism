8d6b900ba7598d2ad7967b6628ad83132c5ed2b8
Théo Pomies <theo.pomies@outlook.com>
2022-03-23 17:52:45 +0100

    feat: refactor: compute bitshift only once

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -4,14 +4,14 @@ pub struct Allergies(u32);
 
 #[derive(Debug, PartialEq, Copy, Clone, IntoEnumIterator)]
 pub enum Allergen {
-    Eggs = 0,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1 << 0,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7,
 }
 
 impl Allergies {
@@ -20,7 +20,7 @@ impl Allergies {
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        self.0 & (1 << *allergen as u32) != 0
+        self.0 & *allergen as u32 != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
@@ -29,3 +29,4 @@ impl Allergies {
             .collect()
     }
 }
+
