c8b9fa511dda972ee2750ea2309881316246f9b7
Théo Pomies <theo.pomies@outlook.com>
2022-03-23 22:31:04 +0100

    feat: add: filtering for edge cases

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -5,6 +5,7 @@ pub fn is_pangram(sentence: &str) -> bool {
     sentence
         .to_ascii_lowercase()
         .chars()
+        .filter(char::is_ascii_alphabetic)
         .collect::<HashSet<_>>()
         .is_superset(&"abcdefghijklmnopqrstuvwyxz".chars().collect())
 }
