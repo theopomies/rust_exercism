84f16bc411fd7d99eacd8d42a435543ed0448a10
Théo Pomies <theo.pomies@outlook.com>
2022-03-25 16:17:18 +0100

    feat: refactor: use native step_by

---
+++ low-power-embedded-game/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.step_by(2)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        let &Position(x, y) = self;
+        i16::abs(x) + i16::abs(y)
+    }
+}
